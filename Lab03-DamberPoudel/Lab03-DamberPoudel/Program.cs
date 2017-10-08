dad3f77780b9abaa9669fb37f107d3cc6abbe74e
DamberPoudel <31965047+DamberPoudel@users.noreply.github.com>
2017-10-08 15:13:53 -0400

    Lab03-DamberPoudel

---
+++ Lab03-DamberPoudel/Lab03-DamberPoudel/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab03_DamberPoudel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declare x y z
+            int x;
+            int y;
+            int z;
+            int sum;
+
+            //assign a value
+            x = 2;
+            y = 2;
+            z = 2;
+            sum = x + y * z + 10;
+
+            //arithmetic operators: + - = * % /
+            sum = x + y * z + 10;
+           
+
+
+
+            {
+
+                Console.WriteLine("Hello\tWorld!");
+                Console.WriteLine("Hello\nWorld");
+                Console.WriteLine("\n\"Hello World!\"");
+                Console.WriteLine("\nHello\\World!");
+
+
+            }
+
+            
+
+
+
+
+            
+
+
+        }
+    }
+}
