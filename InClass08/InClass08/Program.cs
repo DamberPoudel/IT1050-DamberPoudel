b06c25f3a9434675974b525b80039dad87cdabc8
Tri-C Faculty-Staff <Tri-C Faculty-Staff@LT5CG6250Q8R>
2017-10-26 12:44:54 -0400

    InClass08

---
+++ InClass08/InClass08/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InClass08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            for (; i <= 10000; i += 10)
+            {
+            }
+            Console.WriteLine(i);
+
+            i = 1;
+            while (i < 11)
+            {
+                Console.WriteLine(i);
+                i++;
+
+            }
+            //do-while version of while loop
+            //remember it always runs the first time
+            i = 1;
+            do
+
+            {
+                Console.WriteLine(i);
+
+            } while (i < 11);
+
+            //do-while based on user input 
+            string input = "";
+
+            {
+                Console.Write("Enter e to end:");
+                input = Console.ReadLine();
+            }
+
+            {
+                int value = 5;
+                switch (value)
+                    }
+
+            }
+                 case 1:
+                            Console.WriteLine(1);
+                    }
+                }
+            }
+
+            
+
+
+        
+    
+
