d1c95c608a9140017cdddd5619fcfa11d410edfe
Damber Poudel <S01045674@acad.tri-c.edu>
2017-11-09 12:49:59 -0500

    InClass09

---
+++ InClass09/InClass09/MyMath.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InClass09
+{
+    class MyMath
+    {
+        public int result;
+        public int operand1;
+        public int operand2;
+
+        public int Add(int a, int b)
+        {
+            return a + b;
+
+        }
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+        public static int Multiply(int a, int b)
+        {
+            return a * b;
+
+        }
+    }
+}
---
+++ InClass09/InClass09/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InClass09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            calling static methods from within program.cs
+            int sum = AddNumbers(5, 6, 7);
+            Console.WriteLine(sum);
+            */
+
+            //static method call to multiply
+            int product = MyMath.Multiply(7, 3);
+            Console.WriteLine("Product:" + product);
+
+            MyMath mathobj1 = new MyMath();
+            int sum = mathobj1.Add(7, 3);
+
+
+        }
+        
+
+        /*
+        static int AddNumbers(int a, int b)
+        {
+            return a + b;
+        }
+        static int AddNumbers(int a, int b, int c)
+        {
+            return a + b + c;
+
+        }
+
+    }
+}
