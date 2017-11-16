57138ee771a581feb8e3cbba82425251bf58eb52
CristalleGanter <S01059084@acad.tri-c.edu>
2017-11-16 12:43:03 -0500

    InClass10

---
+++ InClass10/InClass10/GermanTranslator.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InClass10
+{
+
+    class GermanTranslator
+    {
+        public void sayHello()
+        {
+            Console.WriteLine("Guten Tag!");
+        }
+
+        public void sayGoodbye()
+        {
+            Console.WriteLine("Auf Wiedersehen");
+        }
+    }
+    public void SayHello(string greetingType)
+    {
+        if (greetingType.ToLower() == "formal")
+        {
+            Console.WriteLine("Wie geht es Ihnen?");
+
+        }
+
+        else
+        {
+            Console.WriteLine("Wie geht es dir?");
+        }
+    }
+    public bool IsEven(int number)
+    {
+        if (number % 2 == 0)
+        {
+            return true;
+
+        }
+        return false;
+
+    }//
+}
+public void sayGoodbye(int greetingtype)
+{
+    if (greetingtype == 1)
+    {
+        Console.WriteLine("Bis Spaeter!");
+
+    }
+    if (greetingtype ==2)
+    {
+        Console.WriteLine("Auf Wiedersehen");
+
+    }
+    if (greetingtype == 3)
+    {
+        Console.WriteLine("");
+    }
+}
---
+++ InClass10/InClass10/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InClass10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GermanTranslator GT = new GermanTranslator();
+            GT.sayHello("formal");
+            GT.sayHello();
+            GT.sayGoodbye(3);
+
+
+        }
+    }
+}
