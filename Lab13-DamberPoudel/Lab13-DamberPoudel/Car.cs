66a13036ad88086f6f6ee9708f3a773658824b50
DamberPoudel <31965047+DamberPoudel@users.noreply.github.com>
2017-12-09 12:21:54 -0500

    Lab13

---
+++ Lab13-DamberPoudel/Lab13-DamberPoudel/Car.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab13_DamberPoudel
+{
+    class Car
+    {
+        private int Speed;
+        private string Make;
+        private string Model;
+        private int Year;
+    }
+}
+        public Car(string make, string model, int year, int speed)
+{
+    this.Make = make;
+    this.Model = model;
+    this.Year = year;
+    this.Speed = speed;
+
+   
+}
+        public car(string make, string model, int year)
+{
+    this.Make = make;
+    this.Model = model;
+    this.Year = year;
+    this.Speed = 0;
+
+}
+
+public int SpeedUp()
+{
+    this.Speed++;
+    return this.speed;
+}
+public int slowdown()
+{
+    this.Speed--;
+    return this.Speed;
+}
